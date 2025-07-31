0501d5a2be25c72607f3706723bc1255601cfeda
Joseph Ogunfowokan <josephoguns5@gmail.com>
2025-07-31 18:14:45 +0100

    Modified technical view for crud operations with the data nest

--- technical/views/crud.py
+++ technical/views/crud.py
@@ -2,11 +2,8 @@ from rest_framework import viewsets
 from technical.models import *
 from technical.serializers import *
 from commercial.date_filters import get_date_range_from_request
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.db.models.functions import TruncMonth
 from commercial.utils import get_filtered_feeders
-from django.db.models import Avg, Max
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
@@ -17,77 +14,7 @@ from django.utils.dateparse import parse_datetime
 from datetime import timedelta
 from django.http import Http404
 import pytz # type: ignore
-from rest_framework.response import Response
-from django.db.models import Avg
-from datetime import datetime, timedelta
-from technical.models import EnergyDelivered, HourlyLoad, FeederInterruption
-from common.models import Feeder
-
-
-def get_average_hours_of_supply(request):
-    feeders = get_filtered_feeders(request)
-    date_from, date_to = get_date_range_from_request(request, 'date')
-
-    qs = DailyHoursOfSupply.objects.filter(feeder__in=feeders)
-    if date_from and date_to:
-        qs = qs.filter(date__range=(date_from, date_to))
-    elif date_from:
-        qs = qs.filter(date__gte=date_from)
-    elif date_to:
-        qs = qs.filter(date__lte=date_to)
-
-    return qs.aggregate(avg=Avg('hours_supplied'))['avg'] or 0
-
-
-def get_average_interruption_duration(request):
-    feeders = get_filtered_feeders(request)
-    date_from, date_to = get_date_range_from_request(request, 'date')
-
-    qs = FeederInterruption.objects.filter(feeder__in=feeders)
-    if date_from and date_to:
-        qs = qs.filter(occurred_at__date__range=(date_from, date_to))
-    elif date_from:
-        qs = qs.filter(occurred_at__date__gte=date_from)
-    elif date_to:
-        qs = qs.filter(occurred_at__date__lte=date_to)
-
-    durations = [(i.restored_at - i.occurred_at).total_seconds() / 3600 for i in qs]
-    return sum(durations) / len(durations) if durations else 0
-
-
-def get_peak_load(request):
-    feeders = get_filtered_feeders(request)
-    date_from, date_to = get_date_range_from_request(request, 'date')
-
-    qs = HourlyLoad.objects.filter(feeder__in=feeders)
-    if date_from and date_to:
-        qs = qs.filter(date__range=(date_from, date_to))
-    elif date_from:
-        qs = qs.filter(date__gte=date_from)
-    elif date_to:
-        qs = qs.filter(date__lte=date_to)
-
-    return qs.aggregate(peak=Max('load_mw'))['peak'] or 0
-
-
-def get_top_or_bottom_loaded_feeders(request, top=True, limit=5):
-    feeders = get_filtered_feeders(request)
-    date_from, date_to = get_date_range_from_request(request, 'date')
-
-    qs = HourlyLoad.objects.filter(feeder__in=feeders)
-    if date_from and date_to:
-        qs = qs.filter(date__range=(date_from, date_to))
-    elif date_from:
-        qs = qs.filter(date__gte=date_from)
-    elif date_to:
-        qs = qs.filter(date__lte=date_to)
-
-    agg = qs.values('feeder__slug', 'feeder__name').annotate(
-        peak_load=Max('load_mw')
-    ).order_by('-peak_load' if top else 'peak_load')[:limit]
-
-    return list(agg)
-
+import datetime
 
 
 class EnergyDeliveredViewSet(viewsets.ModelViewSet):
@@ -528,64 +455,3 @@ class DailyHoursOfSupplyViewSet(viewsets.ModelViewSet):
             qs = qs.filter(date__lte=date_to)
 
         return qs
-
-
-class TechnicalMetricsView(APIView):
-    def get(self, request):
-        top_n = int(request.GET.get('top_n', 5))
-        bottom_n = int(request.GET.get('bottom_n', 5))
-
-        data = {
-            "average_hours_of_supply": round(get_average_hours_of_supply(request), 2),
-            "average_interruption_duration": round(get_average_interruption_duration(request), 2),
-            "peak_load": round(get_peak_load(request), 2),
-            "top_loaded_feeders": get_top_or_bottom_loaded_feeders(request, top=True, limit=top_n),
-            "least_loaded_feeders": get_top_or_bottom_loaded_feeders(request, top=False, limit=bottom_n)
-        }
-        return Response(data)
-
-
-
-class TechnicalMonthlySummaryView(APIView):
-    def get(self, request):
-        feeders = get_filtered_feeders(request)
-        date_from, date_to = get_date_range_from_request(request, 'date')
-
-        supply_qs = DailyHoursOfSupply.objects.filter(feeder__in=feeders)
-        if date_from and date_to:
-            supply_qs = supply_qs.filter(date__range=(date_from, date_to))
-        elif date_from:
-            supply_qs = supply_qs.filter(date__gte=date_from)
-        elif date_to:
-            supply_qs = supply_qs.filter(date__lte=date_to)
-
-        supply_monthly = supply_qs.annotate(month=TruncMonth('date')).values('month').annotate(
-            avg_hours=Avg('hours_supplied')
-        ).order_by('month')
-
-        interruption_qs = FeederInterruption.objects.filter(feeder__in=feeders)
-        if date_from and date_to:
-            interruption_qs = interruption_qs.filter(occurred_at__date__range=(date_from, date_to))
-        elif date_from:
-            interruption_qs = interruption_qs.filter(occurred_at__date__gte=date_from)
-        elif date_to:
-            interruption_qs = interruption_qs.filter(occurred_at__date__lte=date_to)
-
-        data = []
-        for month in supply_monthly:
-            month_date = month['month']
-            avg_hours = month['avg_hours']
-
-            inter_q = interruption_qs.filter(occurred_at__month=month_date.month, occurred_at__year=month_date.year)
-            durations = [
-                (i.restored_at - i.occurred_at).total_seconds() / 3600 for i in inter_q
-            ]
-            avg_interrupt = sum(durations) / len(durations) if durations else 0
-
-            data.append({
-                "month": month_date.strftime("%Y-%m"),
-                "average_hours_of_supply": round(avg_hours, 2) if avg_hours else 0,
-                "average_interruption_duration": round(avg_interrupt, 2),
-            })
-
-        return Response(data)
